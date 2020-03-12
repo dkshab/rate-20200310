40a0e4a79dcc0b9145b9097b300a0a246f3e1d0d
Dumisani <dumisanik.shabangu@gmail.com>
2020-03-12 18:29:40 +0200

    Getting Started

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import "./index.css";
+import Application from "./components/Application";
+
+render(
+  <Router>
+    <Application />
+  </Router>,
+  document.getElementById("root")
+);
